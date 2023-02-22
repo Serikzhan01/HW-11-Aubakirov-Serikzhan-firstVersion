7928e06cd62b042da7b25ff96124b7bf49c12e04
Serikzhan <serikzhan.aubakirov@gmail.com>
2023-02-22 23:22:48 +0600

    added loginTextField and passwordTextField

--- HW-11-Aubakirov-Serikzhan-firstVersion/ViewController.swift
+++ HW-11-Aubakirov-Serikzhan-firstVersion/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// MARK: Extensions
+extension UITextField {
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
 
+class ViewController: UIViewController {
+   
+    // MARK: Outlets
+    private lazy var loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .systemGray2
+        textField.textAlignment = .center
+        textField.placeholder = "keeanurives01"
+        textField.backgroundColor = .systemGray6
+        textField.layer.cornerRadius = 10
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.setLeftIcon(UIImage.init(named: "userIcon") ?? UIImage.remove)
+        return textField
+    }()
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .systemGray2
+        textField.textAlignment = .center
+        textField.placeholder = "Password"
+        textField.backgroundColor = .systemGray6
+        textField.layer.cornerRadius = 10
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.setLeftIcon(UIImage.init(named: "passwordIcon") ?? UIImage.remove)
+        return textField
+    }()
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupConstaints()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: Setup
+    func setupHierarchy() {
+        view.addSubview(loginTextField)
+        view.addSubview(passwordTextField)
+    }
+    
+    func setupConstaints() {
+        NSLayoutConstraint.activate([
+            loginTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -150.0),
+            loginTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 70),
+            loginTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -70),
+            
+            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100.0),
+            passwordTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 70),
+            passwordTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -70)
+        ])
+    }
+    
+    // MARK: Actions
+    
 }
 
